28320073857a37ceda5941202ec2664a22766383
Mi Kim <jaime1118@Jaimes-MacBook-Pro.local>
2019-03-26 20:46:04 -0700

    react functional components prototype completed

--- src/components/app.js
+++ src/components/app.js
@@ -1,2 +1,35 @@
+import React from 'react';
+import Table from './table';
 
+// function App() {
+//     return <h1>My first React functional component</h1>
+// }
 
+// export default App;
+
+export default () => {
+    const students = [
+        {
+            name: 'Harry Potter',
+            course: 'Magic',
+            grade: 84
+        },
+        {
+            name: 'Ron Weasley',
+            course: 'Flying',
+            grade: 78
+        },
+        {
+            name: 'Hermoine Granger',
+            course: 'Potion',
+            grade: 96
+        }
+    ];
+
+    return (
+        <div className = 'container'>
+            <h1>Student Grade Table</h1>
+            <Table data = {students}/>
+        </div>
+    )
+}
---
+++ src/components/table.js
@@ -0,0 +1,43 @@
+import React from 'react';
+
+export default (props) => {
+    const tableRows = props.data.map((item, index) => {
+        return (
+            <tr key={index}>
+                <td>{item.name}</td>
+                <td>{item.course}</td>
+                <td>{item.grade}</td>
+            </tr>
+        )
+    });
+
+    return (
+        <table className = 'table'>
+            <thead className = 'thead-inverse'>
+                <tr>
+                    <th>Name</th>
+                    <th>Course</th>
+                    <th>Grade</th>
+                </tr>
+            </thead>
+            <tbody>
+                {tableRows}
+                {/* <tr>
+                    <td>Mickey Mouse</td>
+                    <td>Dancing</td>
+                    <td>88</td>
+                </tr>
+                <tr>
+                    <td>Minnie Mouse</td>
+                    <td>Singing</td>
+                    <td>92</td>
+                </tr>
+                <tr>
+                    <td>Daisy Duck</td>
+                    <td>Drawing</td>
+                    <td>96</td>
+                </tr> */}
+            </tbody>
+        </table>
+    );
+}
