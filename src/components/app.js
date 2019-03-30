e092c1bec79b4dd86639731a23ce78af53882523
Mi Kim <jaime1118@Jaimes-MacBook-Pro.local>
2019-03-30 14:11:06 -0700

    Finished stopwatch prototype

--- src/components/app.js
+++ src/components/app.js
@@ -1,2 +1,18 @@
+import React from 'react';
+import Stopwatch from './stopwatch';
+
+// function App() {
+
+// }
+
+// export default App;
 
 
+export default (props) => {
+    return (
+        <div className = 'container'>
+            <h1 className = 'text-center'>Stopwatch</h1>
+            <Stopwatch/>
+        </div>
+    )
+}
